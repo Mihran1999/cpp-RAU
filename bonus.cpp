441e67fd0c7c2e895d72018b6bb8e15f4e8e9cac
Mihran Sahakyan <mir_sah@mail.ru>
2016-12-02 18:24:42 +0400

    first cpp push

---
+++ bonus.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<std::vector<int>> v(m, std::vector<int>(n)); //matrix
+	int count = 0;
+	for (int i = 0; i < m; ++i)
+	{
+		for (int k = 0; k < n; ++k)
+		{
+			std::cin >> v[i][k];
+		}
+	}
+
+	for (int i = 0; i < m; ++i)
+	{
+		for (int k = 0; k < n - 1; ++k)
+		{
+			for (int j = k; j < n - 1; ++j)
+			{
+				if (v[i][k] > v[i][j + 1])
+				{
+					++count;
+				}
+			}
+		}
+	}
+	std::cout << count << "\n";
+}
